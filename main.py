aec4f07d66e309dd1dc2cdf7f1fa26142582a400
vedanshipanchal <vedanshipanchal@gmail.com>
2024-10-12 17:43:34 +0100

    Polygon

---
+++ main.py
@@ -0,0 +1,15 @@
+import turtle
+
+turtle.Screen().bgcolor("green")
+turtle.Screen().setup(500,500)
+
+pencil = turtle.Turtle()
+
+for loop in range(6):
+ pencil.forward(100)
+ pencil.right(60)
+ 
+
+
+
+turtle.done()
