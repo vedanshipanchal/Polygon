381113827e0720377669146617d1be60afb3eb76
vedanshipanchal <vedanshipanchal@gmail.com>
2024-10-12 18:04:02 +0100

    Star

--- main.py
+++ main.py
@@ -5,10 +5,32 @@ turtle.Screen().setup(500,500)
 
 pencil = turtle.Turtle()
 
-for loop in range(6):
- pencil.forward(100)
- pencil.right(60)
- 
+#1 triangle
+pencil.forward(200)
+
+pencil.left(120)
+pencil.forward(200)
+
+pencil.left(120)
+pencil.forward(200)
+
+#move pencil
+pencil.penup()
+pencil.right(150)
+pencil.forward(100)
+
+#2 triangle start draw
+pencil.pendown()
+pencil.right(90)
+pencil.forward(200)
+
+pencil.right(120)
+pencil.forward(200)
+
+pencil.right(120)
+pencil.forward(200)
+
+
 
 
 
